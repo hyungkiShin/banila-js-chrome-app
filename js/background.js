d2d702bf8554239bb03b38df48ca96693c7d873f
hyungkiShin <tkaqkeldk@nate.com>
2022-10-24 02:22:33 +0900

    [feat] 졸업작품 완료:

--- js/background.js
+++ js/background.js
@@ -1,9 +1,12 @@
-const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
+const images = ["002.jpeg","001.png"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img");
 
 bgImage.src = `img/${chosenImage}`;
+bgImage.style.width = '1950px';
+bgImage.style.height = '1200px';
+
 
 document.body.appendChild(bgImage);
